a90b63953c1156cf47bf22b9a7e034ab7ae7e13b
Daniel <yoppd@oregonstate.edu>
2021-06-24 22:14:32 -0400

    schedule deadline temp adjust

--- src/ScheduleDeadline.java
+++ src/ScheduleDeadline.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 public class ScheduleDeadline {
     //must complete a job by deadline in order to receive payoff
     //job has a time component and a deadline
+    //want to maximize the payoff
+    //want to maximize the number of jobs
+
+    //sort based on payoff
+    //take shortest jobs with earliest end time
+    
 
 
 
